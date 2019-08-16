bc15a6d63a5f56f10a2e710b21536a59146ddd03
Helen <liaforweb@gmail.com>
2019-08-16 17:28:57 +0300

    header

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 2019-08-imark
+
+<a href="https://lia5.github.io/2019-08-imark/myapp/index.html">Главная</a>
+
---
+++ myapp/js/custom.js
@@ -0,0 +1,61 @@
+$(function() {
+    //lang 
+    $(".lang__current").click(function (){
+        $(".lang__wrap").addClass("active");
+
+    });
+    $(".lang__item").click(function (){
+        var lang = $(this).text();
+        $(".lang__current").text(lang);
+        $(".lang__wrap").removeClass("active");
+    }); 
+
+
+
+
+
+
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+//menu
+    var menu = document.querySelector('.menu-toggle');
+    menu.addEventListener('click', function(){
+        var nav = document.querySelector('.main-menu');
+        nav.classList.toggle('active');
+        var navGamb = document.querySelector('.menu-toggle');
+        navGamb.classList.toggle('active');
+    });
+//tabs
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.header__tabs > li");
+    function myTabClicks(tabClickEvent) {
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+		var clickedTab = tabClickEvent.currentTarget;
+		clickedTab.classList.add("active");
+		tabClickEvent.preventDefault();
+		var myContentPanes = document.querySelectorAll(".tab-pane");
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+        var anchorReference = tabClickEvent.target;
+        console.log(anchorReference);
+        var activePaneId = anchorReference.getAttribute("href");
+        console.log(activePaneId);
+        var activePane = document.querySelector(activePaneId);
+        console.log(activePaneId);
+		activePane.classList.add("active");
+    }
+    for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
+
+
+
+
+
+});
