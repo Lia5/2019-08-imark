b8036011ba7e50c973c0ef8120711a46eeb74b27
Helen <liaforweb@gmail.com>
2019-08-28 15:27:26 +0300

    fix menu

--- myapp/js/custom.js
+++ myapp/js/custom.js
@@ -99,8 +99,34 @@ $(function() {
 
 	$(window).resize(function() {
 		AnimActive();
-	});
-
+    });
+    
+    /*fix menu*/
+    var navPos, navPosEnd, winPos, navHeight;
+    
+    function refreshVar() {
+      try {
+        navPos = $('.header').offset().top;
+        navPosEnd = $('.contacts').offset().top;
+      } catch (e){}
+      navHeight = $('.fix-menu').outerHeight(true);
+    }
+    refreshVar();
+    $(window).resize(refreshVar);
+  
+    $('<div class="clone-fix-menu"></div>').insertBefore('.about').css('height', navHeight).hide();
+      $(window).scroll(function() {
+        winPos = $(window).scrollTop();
+  
+        if ((winPos >= navPos)&&(winPos <= navPosEnd) ) {
+          $('.fix-menu').addClass('fixed shadow');  
+        //   $('.clone-fix-menu').show();
+        } else {
+          $('.fix-menu').removeClass('fixed shadow');
+        //   $('.clone-fix-menu').hide();
+        }
+      });
+    
 });
 
 
