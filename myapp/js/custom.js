f77d3c1dc0254f5ea2f398d7adeda29fd8b6540e
Helen <liaforweb@gmail.com>
2019-08-29 10:45:15 +0300

    services

--- myapp/js/custom.js
+++ myapp/js/custom.js
@@ -100,13 +100,34 @@ $(function() {
 	$(window).resize(function() {
 		AnimActive();
     });
-    
+    /*slider*/
+    if(jQuery('.services__reviews-slider').length) {
+        $('.services__reviews-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        arrows: true,
+        dots: false,
+        prevArrow: '<div class="arrow-prev"></div>',
+        nextArrow: '<div class="arrow-next"></div>',
+        responsive: [{
+            breakpoint: 1240,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          }
+        ]
+        // prevArrow: '<div class="arrow-prev"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z"/></svg></div>',
+        // nextArrow: '<div class="arrow-next"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z"/></svg></div>'
+        });
+    }    
     /*fix menu*/
     var navPos, navPosEnd, winPos, navHeight;
     
     function refreshVar() {
       try {
-        navPos = $('.header').offset().top;
+        navPos = $('section').offset().top;
         navPosEnd = $('.footer').offset().top;
       } catch (e){}
       navHeight = $('.fix-menu').outerHeight(true);
@@ -126,7 +147,7 @@ $(function() {
         //   $('.clone-fix-menu').hide();
         }
       });
-    
+
 });
 
 
